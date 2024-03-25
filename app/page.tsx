71f92a1bee7a7faa9426138717ca369a515952fc
SurajSomkuwar9 <suraj.somkuwar.btech2020@sitpune.edu.in>
2024-03-26 01:51:23 +0530

    wrapping bganimation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import DailySchedule from './pages/DailySchedule';
 import timetableData from './timetable.json';
 import { format, parseISO } from 'date-fns';
+import { BackgroundAnimation } from "./components/ui/Background-Animation";
 
 const IndexPage: React.FC = () => {
   const timetable = timetableData;
@@ -15,15 +16,19 @@ const IndexPage: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Study Planner</h1>
-      <input
-        type="date"
-        value={format(selectedDate, 'yyyy-MM-dd')}
-        onChange={handleDateChange}
-        className="mb-4"
-      />
-      <DailySchedule schedule={timetable} selectedDate={selectedDate} />
+    <div className='wrapper'>
+      <BackgroundAnimation>
+      <div className="container mx-auto p-4" align="center">
+        <h1 className="text-3xl font-bold mb-4">Study Planner</h1>
+        <input
+          type="date"
+          value={format(selectedDate, "yyyy-MM-dd")}
+          onChange={handleDateChange}
+          className="mb-4"
+        />
+        <DailySchedule schedule={timetable} selectedDate={selectedDate} />
+      </div>
+      </BackgroundAnimation>
     </div>
   );
 };
